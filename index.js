db8ba29ce9c90dbde2473c7c13a9255215a5f04b
Rene Hernandez <rene83120@gmail.com>
2022-02-11 11:55:07 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+  const winObject = record.find((object) => object.result === "W");
+  if (winObject) {
+    return winObject.year;
+  } else {
+    return undefined;
+  }
+}
